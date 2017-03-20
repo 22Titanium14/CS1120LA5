3167c0504460fe80f64b1a0b15237bbbbd93fdd3
adamdubs <adamdubs@10.80.100.235>
2017-03-20 12:11:22 -0400

    I completed LA5Main class.

--- CS1120GroupLA5/src/edu/wmich/cs1120/la5/LA5Main.java
+++ CS1120GroupLA5/src/edu/wmich/cs1120/la5/LA5Main.java
@@ -7,11 +7,13 @@
 
 package edu.wmich.cs1120.la5;
 
+import edu.wmich.cs1120.la5.gui.MainWindow;
 
 public class LA5Main {
 
 	public static void main(String[] args) { 
 		// TODO Auto-generated method
+		MainWindow.startGUI();
 	 } 
 
 }
