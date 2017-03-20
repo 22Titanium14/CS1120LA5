1a383f02dac6293369888b254a15b4a7b1baab55
adamdubs <adamdubs@10.80.100.235>
2017-03-20 12:07:39 -0400

    I added our names to each of the class and interface pages.
    Check to make sure I got your names right.

--- CS1120GroupLA5/src/edu/wmich/cs1120/la5/HighArea.java
+++ CS1120GroupLA5/src/edu/wmich/cs1120/la5/HighArea.java
@@ -1,3 +1,10 @@
+/*
+ *Assignment: CS1120 LA5_SP2017
+ *Authors: Adam Dubs, Devin Anderson, Dylan Lafleur
+ *Date: 03/20/2017
+ *Reference: NA
+ */
+
 package edu.wmich.cs1120.la5;
 
 public class HighArea extends Area {
